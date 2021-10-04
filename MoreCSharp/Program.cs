7763481563bd4c4a879f668a5c7487ffef0b32bc
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-04 23:48:53 +0200

    Add Collections.Generic project

---
+++ MoreCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MoreCSharp.Collections.Generic;
+
+namespace MoreCSharp.Tests {
+	public class Program {
+		public static void Main (string[] args) {
+			Console.WriteLine ("Hello World!");
+
+		}
+	}
+}
