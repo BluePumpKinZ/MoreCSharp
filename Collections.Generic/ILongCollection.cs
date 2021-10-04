35e72e13b53f8b5b35a47969d7089083d48a4e46
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-04 23:49:17 +0200

    Add 2 interfaces

---
+++ Collections.Generic/ILongCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MoreCSharp.Collections.Generic {
+
+	public interface ILongCollection<T> : ICollection<T> {
+
+		public long LongCount { get; }
+
+	}
+
+}
---
+++ Collections.Generic/ILongList.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MoreCSharp.Collections.Generic {
+
+	public interface ILongList<T> : ILongCollection<T> {
+
+		public long IndexOf (T item);
+
+		public void Insert (long arrayIndex, T item);
+
+		public void RemoveAt (long arrayIndex);
+
+		public T this[long index] { get; set; }
+
+	}
+
+}
