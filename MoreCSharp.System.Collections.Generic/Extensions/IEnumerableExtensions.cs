e2723bf20233f4aa962d4a5a001204a801d19b5d
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-27 19:20:42 +0200

    Added ForEach function to IEnumerables

---
+++ MoreCSharp.System.Collections.Generic/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MoreCSharp.System.Collections.Generic.Extensions {
+
+	public static class IEnumerableExtensions {
+
+		public static void ForEach<T> (this IEnumerable<T> iEnumerable, Action<T> action) {
+			foreach (T t in iEnumerable) {
+				action.Invoke (t);
+			}
+		}
+
+	}
+}
---
+++ MoreCSharp.System.Collections/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+
+namespace MoreCSharp.System.Collections {
+
+	public static class IEnumerableExtensions {
+
+		public static void ForEach (this IEnumerable iEnumerable, Action<object> action) {
+			foreach (object t in iEnumerable) {
+				action.Invoke (t);
+			}
+		}
+
+	}
+}
