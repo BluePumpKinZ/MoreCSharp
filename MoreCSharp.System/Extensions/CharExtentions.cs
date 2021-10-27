a4bfa755bb304a61a2cceb839051c49e553a771a
Jonas Leijzen <jonasleijzen@gmail.com>
2021-10-27 20:12:44 +0200

    Added string and char repeat functions

---
+++ MoreCSharp.System/Extensions/CharExtentions.cs
@@ -0,0 +1,11 @@
+﻿namespace MoreCSharp.System {
+
+	public static class CharExtentions {
+
+		public static string Repeat (this char c, int amount) {
+			return new string (c, amount);
+		}
+
+	}
+
+}
---
+++ MoreCSharp.System/Extensions/StringExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MoreCSharp.System {
+
+	public static class StringExtensions {
+
+		public static string Repeat (this string str, int amount) {
+			if (str == "")
+				return "";
+			if (amount < 0)
+				throw new ArgumentException ("Amount cannot be negative", nameof (amount));
+			if (str.Length * (long)amount > int.MaxValue)
+				throw new OverflowException ("The resulting string is too big");
+
+			int length = str.Length * amount;
+
+			Span<char> resultSpan = length <= 128 ? stackalloc char[length] : new char[length];
+
+			int strLength = str.Length;
+			
+			for (int i = 0; i < amount; i++) {
+				for (int j = 0; j < strLength; j++) {
+					int offset = i * strLength;
+					resultSpan[offset + j] = str[j];
+				}
+			}
+
+			return new string (resultSpan);
+		}
+
+	}
+
+}
