659d862e54090f66db2f08cac6dbd7c40c233ed3
Jonas Leijzen <jonasleijzen@gmail.com>
2021-11-21 11:19:14 +0100

    Added random dates

---
+++ MoreCSharp.Extensions.System/RandomExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoreCSharp.Extensions.System {
+	public static class RandomExtensions {
+
+		public static DateTime NextDate (this Random random, DateTime maxDate) {
+			return random.NextDate (DateTime.MinValue, maxDate);
+		}
+
+		public static DateTime NextDate (this Random random, DateTime minDate, DateTime maxDate) {
+			long minTicks = minDate.Ticks;
+			long maxTicks = maxDate.Ticks;
+
+			return new DateTime (random.Next (minTicks, maxTicks));
+		}
+
+		public static long Next (this Random random, long maxValue) {
+			return random.Next (0, maxValue);
+		}
+
+		public static long Next (this Random random, long minValue, long maxValue) {
+			if (maxValue < minValue)
+				throw new ArgumentException ("Minvalue must be lower or equal to maxvalue", $"{nameof(minValue)}, {nameof(maxValue)}");
+			byte[] buffer = new byte[8];
+			random.NextBytes (buffer);
+			long randomLong = BitConverter.ToInt64 (buffer, 0);
+			return Math.Abs (randomLong % (maxValue - minValue)) + minValue;
+		}
+
+	}
+}
