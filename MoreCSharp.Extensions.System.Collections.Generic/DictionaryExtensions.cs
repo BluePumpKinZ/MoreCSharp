cf64f9db0e8d284efede4a96aaa11ca2b7f72d70
Jonas Leijzen <jonasleijzen@gmail.com>
2022-02-04 23:12:00 +0100

    MoreCSharp.Extensions.System.Collections.Generic updated to 1.1.0

---
+++ MoreCSharp.Extensions.System.Collections.Generic/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MoreCSharp.Extensions.System.Collections.Generic {
+
+    public static class DictionaryExtensions {
+
+        public static void AddOrUpdate<TKey, TValue> (this Dictionary<TKey, TValue> dictionary, TKey key, TValue value) {
+            if (dictionary.TryAdd (key, value))
+                return;
+            dictionary[key] = value;
+        }
+
+        public static TValue GetOrAdd<TKey, TValue> (this Dictionary<TKey, TValue> dictionary, TKey key) where TValue : new () {
+            if (dictionary.TryGetValue (key, out TValue value))
+                return value;
+            TValue newValue = new TValue ();
+            dictionary.Add (key, newValue);
+            return newValue;
+        }
+
+    }
+
+}
