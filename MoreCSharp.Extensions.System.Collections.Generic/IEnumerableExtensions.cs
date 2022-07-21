45b6e9f150b1090e925a9df71e9632893c620efc
Jonas Leijzen <jonasleijzen@gmail.com>
2022-07-21 16:59:57 +0200

    Added async versions for .ForEach functions for IEnumerables

--- MoreCSharp.Extensions.System.Collections.Generic/IEnumerableExtensions.cs
+++ MoreCSharp.Extensions.System.Collections.Generic/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MoreCSharp.Extensions.System.Collections.Generic {
 
@@ -8,6 +9,12 @@ namespace MoreCSharp.Extensions.System.Collections.Generic {
 		public static void ForEach<T> (this IEnumerable<T> iEnumerable, Action<T> action) {
 			foreach (T t in iEnumerable) {
 				action.Invoke (t);
+			}
+        }
+
+		public static async Task ForEachAsync<T> (this IEnumerable<T> iEnumerable, Func<T, Task> action) {
+			foreach (T t in iEnumerable) {
+				await action.Invoke(t);
 			}
 		}
 
--- MoreCSharp.Extensions.System.Collections/IEnumerableExtensions.cs
+++ MoreCSharp.Extensions.System.Collections/IEnumerableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace MoreCSharp.Extensions.System.Collections {
 
@@ -8,6 +10,12 @@ namespace MoreCSharp.Extensions.System.Collections {
 		public static void ForEach (this IEnumerable iEnumerable, Action<object> action) {
 			foreach (object t in iEnumerable) {
 				action.Invoke (t);
+			}
+		}
+
+		public static async Task ForEachAsync (this IEnumerable iEnumerable, Func<object, Task> action) {
+			foreach (object t in iEnumerable) {
+				await action.Invoke (t);
 			}
 		}
 
